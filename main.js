f35ac39141eb8bb44a446d9aa8d09b480610c1da
ati <ati@localhost.localdomain>
2019-10-27 18:28:20 +0300

    gidhuba yükleme öncesi

--- main.js
+++ main.js
@@ -16,13 +16,14 @@ const {
 } = electron;
 
 let mainWindow;
-
+let addWindow;
 
 app.on('ready', () => {
     console.log(process.platform);
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
+
         }
     });
     mainWindow.loadURL(url.format({
@@ -37,7 +38,14 @@ app.on('ready', () => {
         console.log(data);
     });
     ipcMain.on('key2', (err, data) => {
-        console.log(data);
+        const strdizi = data.split('*');
+        console.log(strdizi[0] + strdizi[1]);
+    });
+    ipcMain.on('key3', () => {
+        yeniPencereAc();
+    });
+    mainWindow.on('close', () => {
+        app.quit();
     });
 });
 const mainMenuTemplate = [{
@@ -80,4 +88,20 @@ if (process.env.NODE_ENV !== "production") {
             }
         ]
     })
+}
+
+function yeniPencereAc() {
+    addWindow = new BrowserWindow({
+        width: 482,
+        height: 150,
+        title: 'penceere başlığı'
+    });
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index2.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    addWindow.on('close', () => {
+        addWindow = null;
+    });
 }
